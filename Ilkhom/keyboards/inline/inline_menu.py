ae685c7a181296b4c51f11b9bd859671a42961cd
Ilkhomkhudjageldiev <ilhom.hudjageldiyev@gmail.com>
2024-01-17 23:18:27 +0500

    Inline BTN added

---
+++ Ilkhom/keyboards/inline/inline_menu.py
@@ -0,0 +1,17 @@
+from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+def menu_btn():
+    menu = InlineKeyboardMarkup(inline_keyboard=[[
+        InlineKeyboardButton(text='EN', callback_data='en'),
+        InlineKeyboardButton(text='RU', callback_data='ru')
+    ]])
+
+    return menu
+
+
+def types_btn():
+    menu = InlineKeyboardMarkup(row_width=1)
+    menu.insert(InlineKeyboardButton(text='EN', callback_data='en'))
+    menu.insert(InlineKeyboardButton(text='RU', callback_data='ru'))
+    return menu
--- Ilkhom/main.py
+++ Ilkhom/main.py
@@ -1,4 +1,5 @@
 import telebot
+from Ilkhom.keyboards.inline.inline_menu import menu_btn
 from Ilkhom.utils.util import content_type_media
 from telebot import types
 from data import config
@@ -6,10 +7,20 @@ from data import config
 bot = telebot.TeleBot(token=config.BOT_TOKEN)
 
 
+@bot.message_handler(commands=['start'])
+def welcome(msg):
+    bot.send_message(msg.chat.id, text='Assalomu alaykum')
+    bot.send_message(msg.chat.id, text='Please select language', reply_markup=menu_btn)
+
+
+
+
 @bot.message_handler(content_types=content_type_media)
-def bot_echo(message):
-    chat_id = message.chat.id
-    bot.copy_message(chat_id, chat_id, message.id)
+def bot_echo(msg):
+    chat_id = msg.chat.id
+    bot.copy_message(chat_id, chat_id, msg.id)
+
+
 
 
 bot.infinity_polling()
